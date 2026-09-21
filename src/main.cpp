70ee61b20b8e186ba31b3488e8d52e451fc2a926
inserti550 <166512415+inserti550@users.noreply.github.com>
2026-09-21 23:02:09 +0300

    delete 'zombie generator' in sh exec

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "globals.hpp"
+#include <csignal>
 
 std::atomic<bool> running         = true;
 
@@ -16,6 +17,7 @@ std::filesystem::path config;
 std::string config_name;
 
 int main(int argc, char* argv[]) {
+    signal(SIGCHLD, SIG_IGN);
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "-c" || arg == "--config") {
